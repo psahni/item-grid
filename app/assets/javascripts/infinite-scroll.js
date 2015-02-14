83a6acc797a517d09242cdb7ccc8553a6d2ffd3b
Prashant <prashant.sahni5@gmail.com>
2015-02-14 20:48:22 +0530

    bug while capturing offset

--- app/assets/javascripts/infinite-scroll.js
+++ app/assets/javascripts/infinite-scroll.js
@@ -1,9 +1,8 @@
-var current_page = 0;
 $(window).scroll(function(){
     if(endOfPage()){
           //alert("This is end of the document");       
-         current_page = current_page + 1;
-         loadContent(current_page);
+         current_page = $("li.active a").text() || 1;
+         loadContent(parseInt(current_page));
     }
 }); 
 
